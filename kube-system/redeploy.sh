84475e78ac52aa0f0476e43cb53858d900787a10
lvpengze <103349015@qq.com>
2020-04-07 11:32:32 +0800

    更改了目录结构

---
+++ kube-system/redeploy.sh
@@ -0,0 +1,11 @@
+sh ./kube_cluster_monitor/delete.sh
+sh ./kube_dashboard/delete.sh
+sh ./kube_dns/delete.sh
+
+# cd kube_dashboard
+# sh deploy.sh
+# cd ../kube_dns
+# sh deploy.sh
+# cd ../kube_cluster_monitor
+# sh deploy.sh
+
